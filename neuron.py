fe05c5316a4ce7876afad18045b8ce4b0fc8d6e5
kesha1225 <48860626+kesha1225@users.noreply.github.com>
2019-10-14 22:42:38 +0300

    neuron to black style

--- neuron.py
+++ neuron.py
@@ -6,8 +6,8 @@ import os
 
 def write_words(*args):
     text, file = args
-    with open(file, 'a') as f:
-        f.write(text + ',')
+    with open(file, "a") as f:
+        f.write(text + ",")
 
 
 async def send_and_gen_sentence(*args):
@@ -15,8 +15,12 @@ async def send_and_gen_sentence(*args):
     if not os.path.exists(file):
         return
     with open(file) as f:
-        text_model = f.read().split(',')
+        text_model = f.read().split(",")
     generator = mc.StringGenerator(learning_data=text_model, order=1)
     words = generator.generate(count=random.randint(1, 10))
-    message = ' '.join(word.lower() for word in words if word.lower() != "m7qumjvv{end}m7qumjvv")
-    await get_api().messages.send(peer_id=peer_id, message=message, random_id=get_random_id())
+    message = " ".join(
+        word.lower() for word in words if word.lower() != "m7qumjvv{end}m7qumjvv"
+    )
+    await get_api().messages.send(
+        peer_id=peer_id, message=message, random_id=get_random_id()
+    )
